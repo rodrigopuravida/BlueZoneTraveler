3ea390045454e2f3a72e5c7a6eead3bd0a379137
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-02 14:19:49 -0800

    Displaying coordinates within ViewDidLoad

---
+++ BlueZoneTraveler/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  BlueZoneTraveler
+//
+//  Created by Rodrigo Carballo on 2/2/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+MKMapView *mapView;
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ BlueZoneTraveler/MapViewController.m
@@ -0,0 +1,61 @@
+//
+//  MapViewController.m
+//  BlueZoneTraveler
+//
+//  Created by Rodrigo Carballo on 2/2/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import "MapViewController.h"
+#import <MapKit/MapKit.h>
+
+@implementation MapViewController
+
+- (IBAction)blueZone1Pressed:(id)sender {
+    
+    NSLog(@"Blue Zone1 clicked");
+   
+    
+}
+
+
+- (IBAction)blueZone2Pressed:(id)sender {
+    
+    NSLog(@"Blue Zone2 clicked");
+}
+
+- (IBAction)blueZone3Pressed:(id)sender {
+    
+    NSLog(@"Blue Zone3 clicked");
+}
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
+    mapView.mapType = MKMapTypeHybrid;
+    
+    CLLocationCoordinate2D coord = {.latitude =  61.2180556, .longitude =  -149.9002778};
+    MKCoordinateSpan span = {.latitudeDelta =  0.2, .longitudeDelta =  0.2};
+    MKCoordinateRegion region = {coord, span};
+    
+    [mapView setRegion:region];
+    [self.view addSubview:mapView];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    
+    [super didReceiveMemoryWarning];
+    
+}
+
+//- (void)dealloc {
+//    
+//    [super dealloc];
+//    
+//}
+
+
+@end

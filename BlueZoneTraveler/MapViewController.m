b8b5d1c814a141163709e02544b29e2d0816dc72
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-02 15:59:05 -0800

    Completed three location feature - day 1

--- BlueZoneTraveler/MapViewController.m
+++ BlueZoneTraveler/MapViewController.m
@@ -15,7 +15,7 @@
 @end
 
 @implementation MapViewController
-
+//TODO: Combine all 3 methods into one
 
 - (IBAction)blueZone1Pressed:(id)sender {
     
@@ -23,41 +23,80 @@
     
     self.mapView.mapType = MKMapTypeHybrid;
     
-    CLLocationCoordinate2D coord = {.latitude =  61.2180556, .longitude =  -149.9002778};
-    MKCoordinateSpan span = {.latitudeDelta =  0.2, .longitudeDelta =  0.2};
+    //Okinawa, Japan
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    
+    CLLocationCoordinate2D coord = {.latitude =  26.5000, .longitude =  128.0000};
+    point.coordinate = coord;
+    [self.mapView addAnnotation:point];
+    MKCoordinateSpan span = {.latitudeDelta =  0.5, .longitudeDelta =  0.5};
+    
     MKCoordinateRegion region = {coord, span};
     
-    [self.mapView setRegion:region];
+    [self.mapView setRegion:region animated:YES];
+    
+
+    
+    //[self.mapView setRegion:region];
     [self.view addSubview:self.mapView];
 
-   
-    
 }
 
 
 - (IBAction)blueZone2Pressed:(id)sender {
     
     NSLog(@"Blue Zone2 clicked");
+    self.mapView.mapType = MKMapTypeHybrid;
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    //Sardinia Italy
+    CLLocationCoordinate2D coord = {.latitude =  40.0000, .longitude =  9.0000};
+    
+    point.coordinate = coord;
+    [self.mapView addAnnotation:point];
+    
+    MKCoordinateSpan span = {.latitudeDelta =  0.5, .longitudeDelta =  0.5};
+    
+    
+    MKCoordinateRegion region = {coord, span};
+    
+    [self.mapView setRegion:region animated:YES];
+    
+    
+    
+    //[self.mapView setRegion:region];
+    [self.view addSubview:self.mapView];
+
 }
 
 - (IBAction)blueZone3Pressed:(id)sender {
     
     NSLog(@"Blue Zone3 clicked");
+    self.mapView.mapType = MKMapTypeHybrid;
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    //Hojancha, Costa Rica
+    CLLocationCoordinate2D coord = {.latitude =  10.1503800, .longitude =  -85.4509300};
+    
+    point.coordinate = coord;
+    [self.mapView addAnnotation:point];
+    
+    
+    MKCoordinateSpan span = {.latitudeDelta =  0.5, .longitudeDelta =  0.5};
+    
+    
+    MKCoordinateRegion region = {coord, span};
+    
+    [self.mapView setRegion:region animated:YES];
+    
+    
+    
+    //[self.mapView setRegion:region];
+    [self.view addSubview:self.mapView];
+
 }
 
 - (void)viewDidLoad {
     
     [super viewDidLoad];
-    
-//    mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
-//    mapView.mapType = MKMapTypeHybrid;
-//    
-//    CLLocationCoordinate2D coord = {.latitude =  61.2180556, .longitude =  -149.9002778};
-//    MKCoordinateSpan span = {.latitudeDelta =  0.2, .longitudeDelta =  0.2};
-//    MKCoordinateRegion region = {coord, span};
-//    
-//    [mapView setRegion:region];
-//    [self.view addSubview:mapView];
     
 }
 

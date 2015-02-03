0b25ca93d3ab2b4b49a59e1df5b40bde43970afc
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-03 14:12:30 -0800

    Added new AddReminderDetailViewController It segues now from pin to the new AddReminderDetailViewController

---
+++ BlueZoneTraveler/AddReminderDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddReminderDetailViewController.h
+//  BlueZoneTraveler
+//
+//  Created by Rodrigo Carballo on 2/3/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddReminderDetailViewController : UIViewController
+
+@end

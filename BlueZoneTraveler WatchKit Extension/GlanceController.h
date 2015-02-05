6782b681549ba8151125b11934efef7fb0c5b9a3
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-05 12:19:38 -0800

    Adding WatchKit Target

---
+++ BlueZoneTraveler WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  BlueZoneTraveler WatchKit Extension
+//
+//  Created by Rodrigo Carballo on 2/5/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ BlueZoneTraveler WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  BlueZoneTraveler WatchKit Extension
+//
+//  Created by Rodrigo Carballo on 2/5/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end

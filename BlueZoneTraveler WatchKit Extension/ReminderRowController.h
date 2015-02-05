91d0afc3a82a282f49e27bd1ef11ec07d3c8d2ae
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-05 12:27:34 -0800

    Added ReminderRowController

---
+++ BlueZoneTraveler WatchKit Extension/ReminderRowController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderRowController.h
+//  BlueZoneTraveler
+//
+//  Created by Rodrigo Carballo on 2/5/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReminderRowController : NSObject
+
+@end
---
+++ BlueZoneTraveler WatchKit Extension/ReminderRowController.m
@@ -0,0 +1,13 @@
+//
+//  ReminderRowController.m
+//  BlueZoneTraveler
+//
+//  Created by Rodrigo Carballo on 2/5/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import "ReminderRowController.h"
+
+@implementation ReminderRowController
+
+@end
